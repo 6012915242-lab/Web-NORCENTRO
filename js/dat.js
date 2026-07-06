56d655ae1c176f141abb56a5a3040664ed2a40ad
abadfloresjhordinjhair-ux <abadfloresjhordinjhair@gmail.com>
2026-07-06 11:52:49 -0500

    creacion de la base de datos para cda moto

---
+++ js/dat.js
@@ -0,0 +1,230 @@
+// ===============================
+// BASE DE DATOS DE MOTOS
+// ===============================
+
+const motos = [
+  {
+    id: 1,
+    nombre: "Bajaj CT 125",
+    marca: "Bajaj",
+    precio: 6500,
+    tipo: "trabajo",
+    imagen: "assets/img/bajaj-ct125.jpg"
+  },
+  {
+    id: 2,
+    nombre: "Bajaj Dominar 400",
+    marca: "Bajaj",
+    precio: 18000,
+    tipo: "deportiva",
+    imagen: "assets/img/dominar400.jpg"
+  },
+  {
+    id: 3,
+    nombre: "Honda CB 190",
+    marca: "Honda",
+    precio: 12000,
+    tipo: "deportiva",
+    imagen: "assets/img/cb190.jpg"
+  },
+  {
+    id: 4,
+    nombre: "Honda CB 190R",
+    marca: "Honda",
+    precio: 13500,
+    tipo: "deportiva",
+    imagen: "assets/img/honda-cb190r.jpg"
+  },
+  {
+    id: 5,
+    nombre: "Honda CRF 300L",
+    marca: "Honda",
+    precio: 25000,
+    tipo: "offroad",
+    imagen: "assets/img/crf300l.jpg"
+  },
+  {
+    id: 6,
+    nombre: "Honda GL 125",
+    marca: "Honda",
+    precio: 5500,
+    tipo: "trabajo",
+    imagen: "assets/img/gl125.jpg"
+  },
+  {
+    id: 7,
+    nombre: "Honda Navi",
+    marca: "Honda",
+    precio: 4800,
+    tipo: "urbana",
+    imagen: "assets/img/navi.jpg"
+  },
+  {
+    id: 8,
+    nombre: "Honda NX 500",
+    marca: "Honda",
+    precio: 28000,
+    tipo: "adventure",
+    imagen: "assets/img/nx500.jpg"
+  },
+  {
+    id: 9,
+    nombre: "Honda XBlade 160",
+    marca: "Honda",
+    precio: 10500,
+    tipo: "deportiva",
+    imagen: "assets/img/xblade160.jpg"
+  },
+  {
+    id: 10,
+    nombre: "Honda XR 150L",
+    marca: "Honda",
+    precio: 9800,
+    tipo: "offroad",
+    imagen: "assets/img/xr150l.jpg"
+  },
+  {
+    id: 11,
+    nombre: "Kawasaki Ninja 400",
+    marca: "Kawasaki",
+    precio: 32000,
+    tipo: "deportiva",
+    imagen: "assets/img/ninja400.jpg"
+  },
+  {
+    id: 12,
+    nombre: "Kawasaki Versys 300",
+    marca: "Kawasaki",
+    precio: 26000,
+    tipo: "adventure",
+    imagen: "assets/img/versys300.jpg"
+  },
+  {
+    id: 13,
+    nombre: "Kawasaki 125",
+    marca: "Kawasaki",
+    precio: 9000,
+    tipo: "urbana",
+    imagen: "assets/img/kawasaki125.jpg"
+  },
+  {
+    id: 14,
+    nombre: "KTM Adventure 390",
+    marca: "KTM",
+    precio: 29000,
+    tipo: "adventure",
+    imagen: "assets/img/adventure390.jpg"
+  },
+  {
+    id: 15,
+    nombre: "KTM Duke 200",
+    marca: "KTM",
+    precio: 14000,
+    tipo: "deportiva",
+    imagen: "assets/img/duke200.jpg"
+  },
+  {
+    id: 16,
+    nombre: "KTM 250",
+    marca: "KTM",
+    precio: 17000,
+    tipo: "deportiva",
+    imagen: "assets/img/ktm250.jpg"
+  },
+  {
+    id: 17,
+    nombre: "Yamaha MT-15",
+    marca: "Yamaha",
+    precio: 13500,
+    tipo: "deportiva",
+    imagen: "assets/img/mt15.jpg"
+  },
+  {
+    id: 18,
+    nombre: "Yamaha FZ 25",
+    marca: "Yamaha",
+    precio: 12500,
+    tipo: "deportiva",
+    imagen: "assets/img/fz25.jpg"
+  },
+  {
+    id: 19,
+    nombre: "Yamaha MT-03",
+    marca: "Yamaha",
+    precio: 22000,
+    tipo: "deportiva",
+    imagen: "assets/img/mt03.jpg"
+  },
+  {
+    id: 20,
+    nombre: "Yamaha NMAX",
+    marca: "Yamaha",
+    precio: 11500,
+    tipo: "urbana",
+    imagen: "assets/img/nmax.jpg"
+  },
+  {
+    id: 21,
+    nombre: "Yamaha R7",
+    marca: "Yamaha",
+    precio: 35000,
+    tipo: "deportiva",
+    imagen: "assets/img/r7.jpg"
+  },
+  {
+    id: 22,
+    nombre: "Yamaha Tenere",
+    marca: "Yamaha",
+    precio: 30000,
+    tipo: "adventure",
+    imagen: "assets/img/tenere.jpg"
+  },
+  {
+    id: 23,
+    nombre: "Yamaha XTZ 125",
+    marca: "Yamaha",
+    precio: 8500,
+    tipo: "offroad",
+    imagen: "assets/img/xtz125.jpg"
+  },
+  {
+    id: 24,
+    nombre: "Yamaha R3",
+    marca: "Yamaha",
+    precio: 15000,
+    tipo: "deportiva",
+    imagen: "assets/img/r3.jpg"
+  },
+  {
+    id: 25,
+    nombre: "Suzuki Gixxer 150",
+    marca: "Suzuki",
+    precio: 9000,
+    tipo: "deportiva",
+    imagen: "assets/img/gixxer150.jpg"
+  },
+  {
+    id: 26,
+    nombre: "Suzuki V-Strom 250",
+    marca: "Suzuki",
+    precio: 24000,
+    tipo: "adventure",
+    imagen: "assets/img/vstrom250.jpg"
+  },
+  {
+    id: 27,
+    nombre: "TVS Apache 160",
+    marca: "TVS",
+    precio: 8500,
+    tipo: "deportiva",
+    imagen: "assets/img/apache160.jpg"
+  },
+  {
+    id: 28,
+    nombre: "Royal Enfield Himalayan",
+    marca: "Royal Enfield",
+    precio: 27000,
+    tipo: "adventure",
+    imagen: "assets/img/himalayan.jpg"
+  }
+];
